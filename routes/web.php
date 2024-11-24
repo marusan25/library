5e51200b94cc8a5bfd26216426ce2a47b43ee218
marusan25 <chibipon2525@gmail.com>
2024-11-24 18:10:45 +0900

    ルートの修正

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\LoginVerify;
 
-Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::middleware(LoginVerify::class)->group(function(){
+    Route::get('/', [HomeController::class,'home'])->name('home');
+
+});
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/login', 'loginCreate')->name('login_create');
+    Route::post('/login', 'loginStore')->name('login_store');
+});

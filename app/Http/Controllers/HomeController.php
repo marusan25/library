2ee823a5d709ea62deefaace1356867100da8a50
marusan25 <chibipon2525@gmail.com>
2024-11-24 18:09:21 +0900

    ホームコントローラーの作成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
-    public function home(Request $requese)
+    public function home(Request $request)
+    {
+        // ログイン後は、ユーザーモデルを auth()->user(); で取得できる
+        return view('home');
+    }
+
+    public function loginCreate(Request $request)
     {
         return view('login');
+    }
+
+    public function loginStore(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if(is_null($user)){
+            // userが見つからなかったとき
+            return to_route('login_create');
+        }
+
+        // userが見つかったとき、パスワードがあってるかどうか
+        if(Hash::check($request->password, $user->password)){
+            auth()->login($user);
+            return to_route('home');
+        }
+
+        return to_route('login_create');
     }
 }

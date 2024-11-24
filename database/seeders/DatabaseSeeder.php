eb5673f1c98d34be8575a1dc7baaf04baafa3875
marusan25 <chibipon2525@gmail.com>
2024-11-24 18:10:04 +0900

    ユーザーのシーダー

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,22 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * https://readouble.com/laravel/11.x/ja/hashing.html
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'テストユーザー',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 0
         ]);
     }
 }

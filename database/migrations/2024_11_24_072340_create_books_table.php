b288765e3367ea1670c5f20a4b93f5b62485ab90
marusan25 <chibipon2525@gmail.com>
2024-11-24 16:43:12 +0900

    マイグレーションファイルの作成

---
+++ database/migrations/2024_11_24_072340_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * https://www.googleapis.com/books/v1/volumes?q=%E3%82%B9%E3%83%83%E3%82%AD%E3%83%AA
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('isbn')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->string('publisher');
+            $table->integer('amount');
+            $table->string('thumbnail_path');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};

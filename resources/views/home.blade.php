11813aae5d5a9e6d88f8ad4fa93d734eda45b10c
marusan25 <chibipon2525@gmail.com>
2024-11-25 22:52:40 +0900

    Top画面のサンプル

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,47 @@
 @endsection
 
 @section('content')
-    <p>いろいろ</p>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    サンプル
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-striped">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                    <th class="small font-weight-bold text-center">列サンプル</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach (range(0, 10) as $i)
+                                <tr>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                    <td class="small">サンプルデータ</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('js')

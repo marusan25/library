37fd71c0de3789cd4e83d6b38ef91225558026dc
YuriIsoda <isoda.yuri0529@gmail.com>
2024-11-26 13:42:39 +0900

    ページネーションの追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator; //追加分
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap(); // 追加分
     }
 }
